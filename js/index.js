5513b2100808ae2dc96eeb71898166f3a941f90d
Fabiola Chazarreta <62580952+fabichazaa@users.noreply.github.com>
2020-04-10 13:33:29 -0300

    taskrunner grunt

---
+++ js/index.js
@@ -0,0 +1,24 @@
+$(function(){
+    		$("[data-toggle='tooltip']").tooltip();
+    		$("[data-toggle='popover']").popover();
+    		$('.carousel').carousel({
+    			interval: 2000
+    		});
+    		$('#contact').on('show.bs.modal', function (e) {
+ 				console.log('Modal is showing');
+ 				$('#contactButton').removeClass('btn-outline-success');
+ 			 	$('#contactButton').addClass('btn-primary');
+ 			 	$('#contactButton').prop('disabled', true);
+	
+			})
+			$('#contact').on('show.bs.modal', function (e) {
+ 				console.log('Modal is shown');
+			})
+			$('#contact').on('hide.bs.modal', function (e) {
+ 				console.log('Modal is hiding');
+			})
+			$('#contact').on('hidden.bs.modal', function (e) {
+ 				console.log('Modal is  hidden');
+ 			 	$('#contactButton').prop('disabled', false);
+			})
+    	});
